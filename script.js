9c0f2d5c43e38d3f85d6a8c5966a2bd9c112971b
Tushar3352G <vermatushar267@gmail.com>
2023-11-13 19:52:01 +0530

    cdn and js updated

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => (t === 1 ? 1 : 1 - Math.pow(2, -10 * t)),
+    direction: "vertical",
+    gestureDirection: "vertical",
+    smooth: true,
+    smoothTouch: false,
+    touchMultiplier: 2,
 });
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 gsap.from(".nlink",{
     stagger:.2,
